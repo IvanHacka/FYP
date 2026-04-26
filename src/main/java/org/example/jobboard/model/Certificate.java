557912528cada09c3babbe67ad256dabb60162f4
N H Ng <nhn7@student.le.ac.uk>
2026-04-26 21:06:05 +0100

    Fixed email alert to inactive user.

---
+++ src/main/java/org/example/jobboard/model/Certificate.java
@@ -0,0 +1,56 @@
+//package org.example.jobboard.model;
+//
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.hibernate.annotations.CreationTimestamp;
+//
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//@Table(name = "ceritficates")
+//public class Certificate {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "certificate_id")
+//    private Long id;
+//
+//    @JsonIgnore
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
+//    @Column(name = "certificate_name", nullable = false)
+//    private String certificateName;
+//
+//    @Column(name = "issuing_organization", nullable = false)
+//    private String issuingOrganization;
+//
+//    @Column(name = "issue_date", nullable = false)
+//    private LocalDate issueDate;
+//
+//    @Column(name = "expiry_date")
+//    private LocalDate expiryDate;
+//
+//    @Column(name = "credential_id")
+//    private String credentialId;
+//
+//    @Column(name = "credential_url")
+//    private String credentialUrl;
+//
+//    @Column(name = "certificate_file")
+//    private String certificateFile;
+//
+//    @CreationTimestamp
+//    @Column(name = "created_at")
+//    private LocalDateTime createdAt;
+//}
